8a679467d47bf7e26ea080f99732e346d3b831f1
BrandonMGH <BrandonMGH@Yahoo.com>
2019-09-27 14:58:34 -0700

    update

---
+++ src/js/components/container/FormContainer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom';
+
+class FormContainer extends Component {
+    state = {
+        title: ""
+    }
+
+    render () {
+        return (
+            <form id="article-form">
+
+            </form>
+        );
+    }
+}
+
+export default FormContainer
